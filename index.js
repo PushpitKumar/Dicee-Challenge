f2ff36fc2e80ab5a5ac2848a22324d1ffe8c6d4b
Pushpit Kumar <83957848+PushpitKumar@users.noreply.github.com>
2024-12-23 18:10:55 +0530

    Updated code

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ var randomNumber2 = Math.random();
 randomNumber2 *= 6;
 randomNumber2 = Math.floor(randomNumber2) + 1;
 
-document.querySelector("img").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
 document.querySelectorAll("img")[0].setAttribute("src", "./images/dice" + randomNumber1 + ".png");
 document.querySelectorAll("img")[1].setAttribute("src", "./images/dice" + randomNumber2 + ".png");
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    document.querySelector(".result").innerHTML = "🚩 Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+    document.querySelector(".result").innerHTML = "🚩 Player 2 Wins!";
 }
 else {
-    document.querySelector("h1").textContent = "Draw!";
+    document.querySelector(".result").textContent = "Draw!";
 }
